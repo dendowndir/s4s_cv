baf6cfcac3503bcf2515fd6cd982eb1be7016b81
opamusora <2003harik2003@gmail.com>
2025-09-26 23:22:41 +0300

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const figtree = Figtree({
+  variable: "--font-figtree",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${figtree.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
